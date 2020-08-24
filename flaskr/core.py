1b16512008a37592749d42aa718aa80d752ab11d
Anish Poudel <anishpoudel007@gmail.com>
2020-08-24 16:31:15 +0545

    Registered budget blueprint in factory function

--- flaskr/core.py
+++ flaskr/core.py
@@ -5,6 +5,8 @@ def create_app():
     app = Flask(__name__)
 
     from .blueprints.public import routes
+    from .blueprints.budget.routes import bp as budget_bp
     app.register_blueprint(routes.bp)
+    app.register_blueprint(budget_bp)
 
     return app
